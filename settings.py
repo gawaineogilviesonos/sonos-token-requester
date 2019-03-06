9203ae7871e0162c26a75464d85f43e36dad826c
gawaineogilviesonos <gawaine.ogilvie@sonos.com>
2019-03-06 12:19:50 -0500

    initial commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+# OAuth Credentials
+CLIENT_ID = "2330236c-5565-448c-9bbb-de3844407d1e"
+CLIENT_SECRET = "508d11b4-d8d7-42d5-bcda-81965f044bfd"
+REDIRECT_URI = "https://0.0.0.0:5000/callback"
+SCOPES_STR = "playback-control-all"
+
+# Sonos Muse API Auth and Token Urls
+OAUTH_URL = "https://api.sonos.com/login/v3/oauth"
+TOKEN_URL = "https://api.sonos.com/login/v3/oauth/access"
+
+REFRESH_TOKEN = "8d432773-6469-4836-80f9-1e55936fb946"
